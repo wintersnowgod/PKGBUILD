4a749b4bdb6731c458508d8a2e12077fa0258394
wintersnowgod <git.xerox732@passinbox.com>
2025-11-02 11:01:52 +0800

    add tailscale-rules pkg

---
+++ tailscale-rules/set-tailscale-udp-gro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "setting tailscale-udp-gro"
+NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
+sudo ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
+tailscale_udp_gro_status=$?
+if [[ $tailscale_udp_gro_status -eq 0 ]]; then
+    echo "success!"
+else
+    echo "failure!"
+fi
