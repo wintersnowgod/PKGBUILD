fceea7bcb563a6ca0ef72662e277a57ae51d33ef
wintersnowgod <git.xerox732@passinbox.com>
2026-01-27 00:25:47 +0800

    reformat using nvim and update checksum

--- tailscale-rules/set-tailscale-udp-gro.sh
+++ tailscale-rules/set-tailscale-udp-gro.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+
 echo "setting tailscale-udp-gro"
 NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
 sudo ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
 tailscale_udp_gro_status=$?
 if [[ $tailscale_udp_gro_status -eq 0 ]]; then
-    echo "success!"
+  echo "success!"
 else
-    echo "failure!"
+  echo "failure!"
 fi
