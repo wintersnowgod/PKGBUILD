2bb042732b6a9440056ce1890d07f13c80447a37
wintersnowgod <git.xerox732@passinbox.com>
2026-01-26 21:10:01 +0800

    change shebang to #!/usr/bin/env bash

--- tailscale-rules/set-tailscale-udp-gro.sh
+++ tailscale-rules/set-tailscale-udp-gro.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "setting tailscale-udp-gro"
 NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
 sudo ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
